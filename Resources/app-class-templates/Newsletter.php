55f47e7f52ef68d52297f02d2595f0390f9d7e00
Jano Paetzold <jep@webfactory.de>
2026-07-30 13:11:55 +0200

    Update Resources/app-class-templates to use attributes instead of annotations (Case 212955)
    Forgotten in fce9de93c9f74d042ce3783caade667560dbf513

--- Resources/app-class-templates/Newsletter.php
+++ Resources/app-class-templates/Newsletter.php
@@ -4,10 +4,8 @@ namespace AppBundle\Newsletter\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity(repositoryClass="\AppBundle\Newsletter\Entity\NewsletterRepository")
- * @ORM\Table("wfd_newsletterNewsletter")
- */
+#[ORM\Entity(repositoryClass: NewsletterRepository::class)]
+#[ORM\Table(name: 'wfd_newsletterNewsletter')]
 class Newsletter extends \Webfactory\NewsletterRegistrationBundle\Entity\Newsletter
 {
 }
--- Resources/app-class-templates/PendingOptIn.php
+++ Resources/app-class-templates/PendingOptIn.php
@@ -4,9 +4,13 @@ namespace AppBundle\Newsletter\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity(repositoryClass="\AppBundle\Newsletter\Entity\PendingOptInRepository")
- */
-class PendingOptIn extends \Webfactory\NewsletterRegistrationBundle\Entity\Recipient
+#[ORM\Entity(repositoryClass: PendingOptInRepository::class)]
+#[ORM\Table(
+    uniqueConstraints: [
+        new ORM\UniqueConstraint(name: 'emailAddressHash_unique', columns: ['emailAddressHash']),
+        new ORM\UniqueConstraint(name: 'uuid_unique', columns: ['uuid']),
+    ]
+)]
+class PendingOptIn extends \Webfactory\NewsletterRegistrationBundle\Entity\PendingOptIn
 {
 }
